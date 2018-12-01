88267714823496bcf23d43e5d948a4e649141580
Jakub Prüher <jacobnzw@gmail.com>
2018-12-01 12:30:31 +0100

    VGG model loading test passes.

--- main.py
+++ main.py
@@ -21,69 +21,135 @@ else:
 def load_vgg(sess, vgg_path):
     """
     Load Pretrained VGG Model into TensorFlow.
-    :param sess: TensorFlow Session
-    :param vgg_path: Path to vgg folder, containing "variables/" and "saved_model.pb"
-    :return: Tuple of Tensors from VGG model (image_input, keep_prob, layer3_out, layer4_out, layer7_out)
+
+    Parameters
+    ----------
+    sess :
+        TensorFlow Session
+
+    vgg_path : str
+        Path to vgg folder, containing "variables/" and "saved_model.pb"
+
+    Returns
+    -------
+    : tuple
+        Tensors from VGG model (image_input, keep_prob, layer3_out, layer4_out, layer7_out)
+
     """
-    # TODO: Implement function
-    #   Use tf.saved_model.loader.load to load the model and weights
+
+    # TODO Use tf.saved_model.loader.load to load the model and weights
     vgg_tag = 'vgg16'
     vgg_input_tensor_name = 'image_input:0'
     vgg_keep_prob_tensor_name = 'keep_prob:0'
     vgg_layer3_out_tensor_name = 'layer3_out:0'
     vgg_layer4_out_tensor_name = 'layer4_out:0'
     vgg_layer7_out_tensor_name = 'layer7_out:0'
-    
-    return None, None, None, None, None
-tests.test_load_vgg(load_vgg, tf)
+    tf.saved_model.loader.load(sess, [vgg_tag], vgg_path)
+    in_ten = sess.graph.get_tensor_by_name(vgg_input_tensor_name)
+    kp_ten = sess.graph.get_tensor_by_name(vgg_keep_prob_tensor_name)
+    l3_ten = sess.graph.get_tensor_by_name(vgg_layer3_out_tensor_name)
+    l4_ten = sess.graph.get_tensor_by_name(vgg_layer4_out_tensor_name)
+    l7_ten = sess.graph.get_tensor_by_name(vgg_layer7_out_tensor_name)
+
+    return in_ten, kp_ten, l3_ten, l4_ten, l7_ten
 
 
 def layers(vgg_layer3_out, vgg_layer4_out, vgg_layer7_out, num_classes):
     """
-    Create the layers for a fully convolutional network.  Build skip-layers using the vgg layers.
-    :param vgg_layer3_out: TF Tensor for VGG Layer 3 output
-    :param vgg_layer4_out: TF Tensor for VGG Layer 4 output
-    :param vgg_layer7_out: TF Tensor for VGG Layer 7 output
-    :param num_classes: Number of classes to classify
-    :return: The Tensor for the last layer of output
+    Create the layers for a fully convolutional network. Build skip-layers using the vgg layers.
+
+    Parameters
+    ----------
+    vgg_layer3_out :
+        TF Tensor for VGG Layer 3 output
+
+    vgg_layer4_out :
+        TF Tensor for VGG Layer 4 output
+
+    vgg_layer7_out :
+        TF Tensor for VGG Layer 7 output
+
+    num_classes :
+        Number of classes to classify
+
+    Returns
+    -------
+    :
+        The Tensor for the last layer of output
     """
+
     # TODO: Implement function
     return None
-tests.test_layers(layers)
 
 
 def optimize(nn_last_layer, correct_label, learning_rate, num_classes):
     """
     Build the TensorFLow loss and optimizer operations.
-    :param nn_last_layer: TF Tensor of the last layer in the neural network
-    :param correct_label: TF Placeholder for the correct label image
-    :param learning_rate: TF Placeholder for the learning rate
-    :param num_classes: Number of classes to classify
-    :return: Tuple of (logits, train_op, cross_entropy_loss)
+
+    Parameters
+    ----------
+    nn_last_layer :
+        TF Tensor of the last layer in the neural network
+
+    correct_label :
+        TF Placeholder for the correct label image
+
+    learning_rate :
+        TF Placeholder for the learning rate
+
+    num_classes :
+        Number of classes to classify
+
+    Returns
+    -------
+    : tuple
+        Tuple of (logits, train_op, cross_entropy_loss)
+
     """
     # TODO: Implement function
     return None, None, None
-tests.test_optimize(optimize)
 
 
 def train_nn(sess, epochs, batch_size, get_batches_fn, train_op, cross_entropy_loss, input_image,
              correct_label, keep_prob, learning_rate):
     """
     Train neural network and print out the loss during training.
-    :param sess: TF Session
-    :param epochs: Number of epochs
-    :param batch_size: Batch size
-    :param get_batches_fn: Function to get batches of training data.  Call using get_batches_fn(batch_size)
-    :param train_op: TF Operation to train the neural network
-    :param cross_entropy_loss: TF Tensor for the amount of loss
-    :param input_image: TF Placeholder for input images
-    :param correct_label: TF Placeholder for label images
-    :param keep_prob: TF Placeholder for dropout keep probability
-    :param learning_rate: TF Placeholder for learning rate
+
+    Parameters
+    ----------
+    sess :
+        TF Session
+
+    epochs :
+        Number of epochs
+
+    batch_size :
+        Batch size
+
+    get_batches_fn :
+        Function to get batches of training data. Call using get_batches_fn(batch_size)
+
+    train_op :
+        TF Operation to train the neural network
+
+    cross_entropy_loss :
+        TF Tensor for the amount of loss
+
+    input_image :
+        TF Placeholder for input images
+
+    correct_label :
+        TF Placeholder for label images
+
+    keep_prob :
+        TF Placeholder for dropout keep probability
+
+    learning_rate :
+        TF Placeholder for learning rate
+
     """
     # TODO: Implement function
     pass
-tests.test_train_nn(train_nn)
 
 
 def run():
@@ -110,6 +176,7 @@ def run():
         #  https://datascience.stackexchange.com/questions/5224/how-to-prepare-augment-images-for-neural-network
 
         # TODO: Build NN using load_vgg, layers, and optimize function
+        load_vgg(sess, vgg_path)
 
         # TODO: Train NN using the train_nn function
 
@@ -118,6 +185,11 @@ def run():
 
         # OPTIONAL: Apply the trained model to a video
 
+
+tests.test_load_vgg(load_vgg, tf)
+# tests.test_layers(layers)
+# tests.test_optimize(optimize)
+# tests.test_train_nn(train_nn)
 
 if __name__ == '__main__':
     run()
